3429612294a448af86c0bcfe7f2961f16632e0ab
farhod <sagatov-99@mail.ru>
2022-03-14 21:35:03 +0500

    d

--- pages/_app.js
+++ pages/_app.js
@@ -72,15 +72,15 @@ function MyApp({Component, pageProps}) {
                 <script
                     dangerouslySetInnerHTML={{
                       __html: `
-                    (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
                     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                    ym(87728552, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-                    });
+                     ym(87728552, "init", {
+                     clickmap:true,
+                     trackLinks:true,
+                     accurateTrackBounce:true,
+                     webvisor:true
+                     });
                         `,
                     }}
                 />
